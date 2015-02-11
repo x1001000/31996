3f364449a7478052ba287b459207e6d0c4bdbb89
x1001000 <x1001000@gmail.com>
2015-02-11 10:07:57 +0800

    jobs@apple.com

---
+++ passbox.sh
@@ -0,0 +1,18 @@
+while(true)
+do
+  PASSWORD=$(whiptail --title "\"__\"" --inputbox "\nMirror mirror on the wall..." 9 32 3>&1 1>&2 2>&3)
+  exitstatus=$?
+  if [ $exitstatus = 0 ]; then
+    if [ $PASSWORD = "31996" ]; then
+      clear
+      cat passbox.1.txt && sleep 2 &&
+      cat passbox.2.txt && sleep 2 &&
+      cat passbox.3.txt && sleep 2 &&
+      cat passbox.4.txt && sleep 4 &&
+      cat passbox.5.txt && sleep 4 &&
+      break
+    fi
+    cmatrix -C magenta -s && sh passbox.sh
+    break
+  fi
+done
